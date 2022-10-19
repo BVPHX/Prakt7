c72606ef866180d51879ed249645834f9b1001e8
TemaYellow RP <temayellow@gmail.com>
2022-10-20 00:21:56 +0300

    Добавьте файлы проекта.

---
+++ Prakt7/Prakt7/InputTable.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prakt7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InputTable : ContentPage
+    {
+        public (string name, string surname, string middleName, string gender, DateTime birthDate, bool isLeader, bool needRoom, int rpmMark, int rmpMark, int trpoMark) user;
+        public InputTable()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUserInfo_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                user.name = nameEntry.Text;
+                user.surname = surnameEntry.Text;
+                user.middleName = middleNameEntry.Text;
+                user.gender = gender.SelectedItem.ToString();
+                user.birthDate = agePicker.Date;
+                if (isLeader.SelectedItem == "Да") user.isLeader = true;
+                else user.isLeader = false;
+                if (isLeader.SelectedItem == "Да") user.isLeader = true;
+                else user.isLeader = false;
+            }
+            catch
+            {
+                DisplayAlert("Ошибка", "Введены неверные данные", "Отмена");
+                return;
+            }
+            
+        }
+        public void AddMarks(string a, string b, string c)
+            {
+            user.rmpMark = Convert.ToInt32(a);
+            user.rpmMark = Convert.ToInt32(b);
+            user.trpoMark = Convert.ToInt32(c);
+            }
+    }
+}
---
+++ Prakt7/Prakt7/MarksPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prakt7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MarksPage : ContentPage
+    {
+        public MarksPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MarksAdd_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                InputTable.AddMarks(rmpMark.Text, rpmMark.Text, trpoMark.Text);
+            }
+            catch 
+            {
+                DisplayAlert("Ошибка", "Введены неверные данные", "Отмена");
+                return;
+            }
+        }
+    }
+}
